9a7ef40be52b375ce6b62ad823e574dbb496a3d7
siam-26 <siamshahriar390@gmail.com>
2023-01-11 12:32:42 +0600

    changed

--- src/Siam/Siam.tsx
+++ src/Siam/Siam.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Siam = () => {
     return (
         <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, similique!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, similiquef!</p>
         </div>
     );
 };
