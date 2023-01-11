6c7d73fa73356cb6331e5ba4964e591b4915f031
siam-26 <siamshahriar390@gmail.com>
2023-01-11 11:46:59 +0600

    siam

---
+++ src/Siam/Siam.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Siam = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, similique!</p>
+        </div>
+    );
+};
+
+export default Siam;
