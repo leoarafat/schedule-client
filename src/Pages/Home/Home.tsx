4b0b1d74536b2517249ddc086f51eac4d374a01b
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2023-01-11 20:14:25 +0600

    modify

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header/Header';
 import Membership from './Membership/Membership';
+import Services from './Services/Services';
 
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <Services></Services>
             <Membership></Membership>
         </div>
     );
