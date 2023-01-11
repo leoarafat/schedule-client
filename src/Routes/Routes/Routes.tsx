229526714730e376cd6fe21ec008a087f3c2b5cf
moddas311 <moddasir311@gmail.com>
2023-01-11 15:57:57 +0600

    routes added and main layout too

---
+++ src/Routes/Routes/Routes.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layouts/Main/Main";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main />,
+        // errorElement:
+        children: [
+
+        ]
+    }
+]);
