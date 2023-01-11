84353f1e2bc2e749c25ad0e378aac68c81a37526
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2023-01-11 16:20:24 +0600

    modify

--- src/Routes/Routes/Routes.tsx
+++ src/Routes/Routes/Routes.tsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layouts/Main/Main";
+import Home from "../../Pages/Home/Home";
+import Membership from "../../Pages/Home/Membership/Membership";
 
 export const router = createBrowserRouter([
     {
@@ -7,6 +9,14 @@ export const router = createBrowserRouter([
         element: <Main />,
         // errorElement:
         children: [
+            {
+                path:'/',
+                element: <Home></Home>
+            },
+            {
+                path:'/membership',
+                element: <Membership></Membership>
+            }
 
         ]
     }
