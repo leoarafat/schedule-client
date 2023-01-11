472f4ef31281d07fc6a3deba787663832101e476
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2023-01-11 18:55:38 +0600

    mm

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import SignUp from '../SignIn/SignIn';
 import Header from './Header/Header';
 import Membership from './Membership/Membership';
 
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Header></Header>
             <Membership></Membership>
+            <SignUp/>
         </div>
     );
 };
