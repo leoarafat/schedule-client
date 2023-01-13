2b009a4dab90d2a58e90ad701efe0b72ff857b75
Syeda-Aisha-Ahmad <18aishaahmad@gmail.com>
2023-01-13 12:08:28 +0600

    color changes nav and header section

--- src/Pages/Home/Header/Header.tsx
+++ src/Pages/Home/Header/Header.tsx
@@ -5,12 +5,12 @@ const Header = () => {
         <div className="hero mt-5 max-w-[1300px] mx-auto">
             <div className="p-2 flex hero-content flex-col lg:flex-row-reverse">
                 <div className='lg:w-5/12 ml-auto'>
-                    <img src="https://img.freepik.com/free-vector/calendar-icon-white-background_1308-84634.jpg?w=2000" className="rounded-lg w-full pt-5" />
+                    <img src="https://i.ibb.co/T82zkZL/Timeline-bro.png" className="rounded-lg w-full pt-5" />
                 </div>
                 <div className='lg:w-6/12 text-center lg:text-left mr-auto'>
-                    <h1 className=" text-5xl font-bold">Lorem ipsum</h1>
-                    <p className="py-6">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-                    <button className="btn btn-error mt-5">Get Started</button>
+                    <h1 className=" text-5xl font-bold">Lorem <span className='text-primary'>ipsum</span></h1>
+                    <p className="py-6 text-accent">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
+                    <button className="btn btn-secondary text-white mt-5">Get Started</button>
                 </div>
             </div>
         </div>
--- src/Shared/Nav/Nav.tsx
+++ src/Shared/Nav/Nav.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Nav.css'
 
 const Nav = () => {
     return (
         <nav className="navbar bg-base-100 flex items-center mt-5 max-w-[1300px] mx-auto">
             <div className="navbar-start">
-                <a className="btn btn-ghost normal-case text-3xl text-error pb-2 btn rounded-full bg-error">o</a>
+                <img src="https://i.ibb.co/MSsVHm1/logo.png" className='w-6/12' alt="" />
             </div>
 
             {/* Large Screen nav items */}
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1 ul-style text-error">
+                <ul className="menu menu-horizontal px-1 ul-style text-primary">
                     <li><a className="no-hover t">Item 1</a></li>
                     <li><a className="no-hover t">Item 3</a></li>
                     <li><a className="no-hover t">Item 1</a></li>
@@ -24,19 +25,19 @@ const Nav = () => {
             <div className="navbar-end">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="text-primary h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content right-1 mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                         <li><a>Item 1</a></li>
                         <li><a>Item 3</a></li>
                         <li><a>Item 1</a></li>
                         <li><a>Item 3</a></li>
-                        <a className="btn">Get started</a>
+                        <li><Link to={"/"} className="btn border-0 lg:flex rounded-full px-10">Sign In</Link></li>
                     </ul>
                 </div>
                 {/* Small Screen nav items */}
 
-                <a className="btn hidden lg:flex btn-error text-white rounded-full">Get started</a>
+                <Link to={""} className="btn hidden border-0 lg:flex bg-primary rounded-full text-white px-10">Sign In</Link>
             </div>
         </nav>
     );
