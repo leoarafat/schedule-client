71bae8585228631efe11a815b51e9325f038fb61
moddas311 <moddasir311@gmail.com>
2023-01-20 18:45:14 +0600

    schedule route added

--- src/Routes/Routes/Routes.tsx
+++ src/Routes/Routes/Routes.tsx
@@ -1,7 +1,7 @@
+import { element } from "prop-types";
 import { createBrowserRouter } from "react-router-dom";
 import Check from "../../Dashboard/Check/Check";
 import Dashboard from "../../Dashboard/Dashboard/Dashboard";
-
 import DashboardLayout from "../../Dashboard/DashboardLayout/DashboardLayout";
 import Main from "../../Layouts/Main/Main";
 import About from "../../Pages/About/About";
@@ -10,9 +10,11 @@ import BLogPost from "../../Pages/Blog/BLogPost";
 import Home from "../../Pages/Home/Home";
 import Plans from "../../Pages/Home/Membership/Plans";
 import Profile from "../../Pages/Profile/Profile";
+import ScheduleInfo from "../../Pages/Schedule/ScheduleInfo/ScheduleInfo";
 import SignIn from "../../Pages/SignIn/SignIn";
 import SignUp from "../../Pages/SignUp/SignUp";
 import ErrorPage from "../../Shared/ErrorPage/ErrorPage";
+
 
 
 
@@ -58,6 +60,10 @@ export const router = createBrowserRouter([
                 path: '/signUp',
                 element: <SignUp></SignUp>
             },
+            {
+                path: '/schedule',
+                element: <ScheduleInfo />
+            }
         ]
     },
     {
