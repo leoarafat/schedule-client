4c89d7c9a839ada5d413a09932b52828592eed20
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2023-01-20 17:46:14 +0600

    modify

--- src/Pages/Home/Membership/Pricing.js
+++ src/Pages/Home/Membership/Pricing.js
@@ -1,131 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import PricingCard from "./PricingCard";
 
 const Pricing = () => {
+  const { data: membership = [] } = useQuery({
+    queryKey: ["membership"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/membership");
+      const data = await res.json();
+      return data;
+    },
+  });
+  console.log(membership);
+
   return (
     <div>
       <div className="bg-gradient-to-b from-blue-100 to-purple-200">
         <div className="container m-auto px-6 py-20 md:px-12 lg:px-20">
-          <div className="m-auto text-center lg:w-8/12 xl:w-7/12">
+          <div className="m-auto text-center">
             <h2 className="text-2xl text-pink-900 font-bold md:text-6xl">
               Pick the perfect plan for your team
             </h2>
           </div>
-          <div className="mt-12 m-auto -space-y-4 items-center justify-center md:flex md:space-y-0 md:-space-x-4 xl:w-10/12 gap-14">
-            <div className="relative group md:w-6/12 md:mx-0 lg:w-5/12">
-              <div
-                aria-hidden="true"
-                className="absolute top-0 w-full h-full rounded-2xl bg-sky-200 shadow-xl transition duration-500 group-hover:scale-105 lg:group-hover:scale-110"
-              ></div>
-              <div className="relative p-6 space-y-6 lg:p-8">
-                <h3 className="text-3xl text-white font-semibold text-center bg-slate-900 w-full p-5">
-                  Basic
-                </h3>
-                <div>
-                  <div className="relative flex justify-around">
-                    <div className="flex items-end">
-                      <span className="text-8xl text-gray-800 font-bold leading-0">
-                        00
-                      </span>
-                      <div className="pb-2">
-                        <span className="block text-2xl text-gray-700 font-bold">
-                          $
-                        </span>
-                        <span className="block text-xl text-purple-500 font-bold">
-                          /month
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <ul
-                  role="list"
-                  className="w-max space-y-4 py-6 m-auto text-gray-600"
-                >
-                  <li className="space-x-2 flex items-center">
-                    <i className="fa-solid fa-check-double"></i>
-
-                    <span>First premium advantage</span>
-                  </li>
-                  <li className="space-x-2 flex items-center">
-                    <i className="fa-solid fa-check-double"></i>
-
-                    <span>Second advantage weekly</span>
-                  </li>
-                  <li className="space-x-2 flex items-center">
-                    <i className="fa-solid fa-check-double"></i>
-
-                    <span>Third advantage donate to project</span>
-                  </li>
-                </ul>
-                <Link to={"/"}>
-                  <button
-                    type="submit"
-                    title="Submit"
-                    className="block w-full py-3 px-6 text-center rounded-xl transition bg-purple-600 hover:bg-purple-700 active:bg-purple-800 focus:bg-indigo-600"
-                  >
-                    <span className="text-white font-semibold">
-                      Get started
-                    </span>
-                  </button>
-                </Link>
-              </div>
-            </div>
-
-            <div className="relative group md:w-6/12 md:mx-0 lg:w-5/12">
-              <div
-                aria-hidden="true"
-                className="absolute top-0 w-full h-full rounded-2xl bg-green-200 shadow-xl transition duration-500 group-hover:scale-105 lg:group-hover:scale-110"
-              ></div>
-              <div className="relative p-6 space-y-6 lg:p-8">
-                <h3 className="text-3xl text-white font-semibold text-center bg-amber-600 w-full p-5">
-                  Premium
-                </h3>
-                <div>
-                  <div className="relative flex justify-around">
-                    <div className="flex items-end">
-                      <span className="text-8xl text-gray-800 font-bold leading-0">
-                        08
-                      </span>
-                      <div className="pb-2">
-                        <span className="block text-2xl text-gray-700 font-bold">
-                          $
-                        </span>
-                        <span className="block text-xl text-purple-500 font-bold">
-                          /month
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <ul
-                  role="list"
-                  className="w-max space-y-4 py-6 m-auto text-gray-600"
-                >
-                  <li className="space-x-2 flex items-center">
-                    <i className="fa-solid fa-check-double"></i>
-                    <span>First premium advantage</span>
-                  </li>
-                  <li className="space-x-2 flex items-center">
-                    <i className="fa-solid fa-check-double"></i>
-                    <span>Second advantage weekly</span>
-                  </li>
-                  <li className="space-x-2 flex items-center">
-                    <i className="fa-solid fa-check-double"></i>
-                    <span>Third advantage donate to project</span>
-                  </li>
-                </ul>
-                <Link to={"/"}>
-                  <button
-                    type="submit"
-                    title="Submit"
-                    className="block w-full py-3 px-6 text-center rounded-xl transition bg-purple-600 hover:bg-purple-700 active:bg-purple-800 focus:bg-indigo-600"
-                  >
-                    <span className="text-white font-semibold">Subscribe</span>
-                  </button>
-                </Link>
-              </div>
-            </div>
+          <div className="grid grid-cols-1 md:grid-cols-2">
+            {membership.map((plan) => (
+              <PricingCard plan={plan} key={plan._id}></PricingCard>
+            ))}
           </div>
         </div>
 
