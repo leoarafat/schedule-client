3fe9170297a2c7ba0c1515d79c845b3e37a4e595
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2023-02-01 12:53:16 +0600

    modify

--- src/Dashboard/Dashboard/Billing/Payment.tsx
+++ src/Dashboard/Dashboard/Billing/Payment.tsx
@@ -1,7 +1,14 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import { useContext } from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import { AuthContext } from "../../../components/Contexts/AuthProvider/AuthProvider";
+import CheckoutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe(
+  "pk_test_51MWZFIFOxg23fb3FZBBESyKsXoFGupdLPx5f3uA7cpivPtneEBABjv7hxFfGt6P0IwRN0O41bNhMtE50ZEiSef2300iMifsqAI"
+);
 
 const Payment = () => {
   const params = useParams();
@@ -28,7 +35,17 @@ const Payment = () => {
       {/* <h1 className="text-center text-xl mt-10">
         You are on <span className="font-bold">{membership?.status}</span> Plan
       </h1> */}
-      <h1 className="text-center text-xl mt-10">Please Pay <span className="font-bold text-2xl">${membership?.cost}</span> to upgrade <span className="font bold text-2xl">{membership?.status}</span></h1>
+      <h1 className="text-center text-xl mt-10">
+        Please Pay{" "}
+        <span className="font-bold text-2xl">${membership?.cost}</span> to
+        upgrade <span className="font bold text-2xl">{membership?.status}</span>
+      </h1>
+
+      <div className="my-10">
+        <Elements stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      </div>
     </div>
   );
 };
