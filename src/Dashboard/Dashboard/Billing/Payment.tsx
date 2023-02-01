0830f55e362f22a63bdb5c1a311e93bc17d3859d
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2023-02-01 11:36:57 +0600

    modify

--- src/Dashboard/Dashboard/Billing/Payment.tsx
+++ src/Dashboard/Dashboard/Billing/Payment.tsx
@@ -1,9 +1,13 @@
-import { useQuery } from 'react-query';
-import { useParams } from 'react-router-dom';
+import { useContext } from "react";
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import { AuthContext } from "../../../components/Contexts/AuthProvider/AuthProvider";
 
 const Payment = () => {
-    const params = useParams();
-    const id = params.id;
+  const params = useParams();
+  const id = params.id;
+
+  const { user }: any = useContext(AuthContext);
 
   const { data: membership = [] } = useQuery({
     queryKey: ["membership"],
@@ -16,14 +20,17 @@ const Payment = () => {
     },
   });
   console.log(membership);
-    return (
-        <div>
-            {
-                membership &&
-                <h1>{membership.status}</h1>
-            }
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-6xl font-bold text-center uppercase">
+        Pay<span className="text-primary">Now</span>
+      </h1>
+      {/* <h1 className="text-center text-xl mt-10">
+        You are on <span className="font-bold">{membership?.status}</span> Plan
+      </h1> */}
+      <h1 className="text-center text-xl mt-10">Please Pay <span className="font-bold text-2xl">${membership?.cost}</span> to upgrade <span className="font bold text-2xl">{membership?.status}</span></h1>
+    </div>
+  );
 };
 
 export default Payment;
