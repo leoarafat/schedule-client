fdaa719109c5d017f3fa144ec13a7f42569e6cd5
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2023-02-08 16:47:31 +0600

    modify

--- src/AddNote/Notes.tsx
+++ src/AddNote/Notes.tsx
@@ -66,67 +66,35 @@ const Notes = () => {
 
         <div className="overflow-y-auto h-[330px]">
           {notes.map((note: any) => (
-            // <div className="border border-gray-500 hover:bg-blue-200 py-5 px-2 md:px-10 rounded-lg md:m-3">
-            //   <div className="text-lg flex items-center justify-between">
-            //     <label htmlFor="my-modal-3" className="w-full cursor-pointer">
-            //       <p>{note.title}</p>
-            //     </label>
-            //     <span className="flex items-center text-lg">
-            //       {/* 11/12/2023{" "} */}
-            //       <RiDeleteBin6Line
-            //         onClick={() => handleDelete(note._id)}
-            //         className="text-3xl ml-5 hover:text-red-700 cursor-pointer"
-            //       />
-            //     </span>
-            //   </div>
-            //   <input type="checkbox" id="my-modal-3" className="modal-toggle" />
-            //   <div className="modal">
-            //     <div className="modal-box relative">
-            //       <label
-            //         htmlFor="my-modal-3"
-            //         className="btn btn-sm btn-circle absolute right-2 top-2"
-            //       >
-            //         ✕
-            //       </label>
-            //       <p className="text-2xl font-bold mt-5">{note.title}</p>
-            //       <p className="py-10">{note.note}</p>
-            //     </div>
-            //   </div>
-            // </div>
-
-            <ul className="p-4 lg:p-8">
+            <ul className="p-4 lg:p-2">
               <li>
                 <label
                   htmlFor="my-modal-3"
                   className="flex items-center justify-between"
                 >
-                  <div className="grid p-4 overflow-hidden rounded-xl lg:p-6  hover:dark:bg-gray-900 hover:text-white">
-                    <h3 className="mb-5 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9 text-xl">
-                      {note.title}
-                    </h3>
-                    <p className="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 ">
-                      {note.note ? (
-                        <>{note.note.slice(0, 100) + "...."}</>
-                      ) : (
-                        note.note
-                      )}
-                    </p>
+                  <div className="flex md:items-center justify-between hover:dark:bg-gray-900 hover:text-white border p-4 overflow-hidden rounded-xl lg:p-6">
+                    <div className="">
+                      <h3 className="mb-5 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9 text-xl">
+                        {note.title}
+                      </h3>
+                      <p className="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 ">
+                        {note.note ? (
+                          <>{note.note.slice(0, 100) + "...."}</>
+                        ) : (
+                          note.note
+                        )}
+                      </p>
+                    </div>
+                    <div className="flex md:items-center text-lg justify-end">
+                      <RiDeleteBin6Line
+                        onClick={() => handleDelete(note._id)}
+                        className="text-3xl ml-8 hover:text-red-700 cursor-pointer"
+                      />
+                    </div>
                   </div>
-                  <div className="flex items-center text-lg justify-end">
-                    <RiDeleteBin6Line
-                      onClick={() => handleDelete(note._id)}
-                      className="text-3xl ml-5 hover:text-red-700 cursor-pointer"
-                    />
-                  </div>
-
-                  
                 </label>
               </li>
-              <input
-                    type="checkbox"
-                    id="my-modal-3"
-                    className="modal-toggle"
-                  />
+              <input type="checkbox" id="my-modal-3" className="modal-toggle" />
               <div className="modal">
                 <div className="modal-box relative">
                   <label
